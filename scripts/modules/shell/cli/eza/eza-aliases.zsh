f50d56b63fe3ed6f627e171ad87df19220c77c3f
Joseph Ison <josephisom@gmail.com>
2026-03-17 22:13:05 -0400

    updated aliases

--- scripts/modules/shell/cli/eza/eza-aliases.zsh
+++ scripts/modules/shell/cli/eza/eza-aliases.zsh
@@ -3,5 +3,6 @@
 alias ls='eza --icons=auto'
 alias l='eza -1'
 alias la='eza -la --icons=auto'
-alias ll='eza -lh --git --icons=auto'
+# alias ll='eza -lh --git --icons=auto'
 alias lt='eza --tree --level=2 --icons=auto'
+alias ll='eza --all --oneline --long --header --inode --flags --bytes --group --modified --time-style="+%Y-%m-%d %H:%M:%S" --group-directories-first --sort=name --color=always --icons=auto --no-quotes --group --git --git-repos --mounts'
